31036d0f933fe6f1da4a0c6c18d1ab089c82e3d9
German <sehenqq@gmail.com>
2020-06-16 23:17:45 +0200

    Add basic styles and buttons load

--- src/components/Scene/index.tsx
+++ src/components/Scene/index.tsx
@@ -1,24 +1,53 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { ScenePropsType } from '../../types/types';
+import styles from './styles.module.scss';
 
-export default class Scene extends Component<ScenePropsType, {}> {
-  handleClick = (id: string) => () => {
-    this.props.nextScene(id);
+let wordsInterval: number;
+let wordsIntervalIndex: number = 0;
+
+export default function Scene({ scene, nextScene }: ScenePropsType) {
+  const [words, setWords] = useState(['']);
+  const [isButtonsVisible, setButtonsVisible] = useState(false);
+  const { image, text, buttons } = scene;
+  const handleClick = (id: string) => () => {
+    nextScene(id);
   };
 
-  render() {
-    const { image, text, buttons } = this.props.scene;
+  const lazyWords = useCallback(() => {
+    const splitedText = text.split(' ');
 
-    return (
-      <div>
-        <img src={image} alt="Background" />
-        <p>{text}</p>
-        {buttons.map(({ text, redirectId }) => (
-          <button key={`${text}${redirectId}`} onClick={this.handleClick(redirectId)}>
-            {text}
+    wordsInterval = window.setInterval(() => {
+      setWords((words) => [...words, splitedText[wordsIntervalIndex]]);
+      wordsIntervalIndex++;
+
+      if (wordsIntervalIndex >= splitedText.length) {
+        clearInterval(wordsInterval);
+        setButtonsVisible(true);
+      }
+    }, 300);
+  }, [text]);
+
+  useEffect(() => {
+    lazyWords();
+
+    return () => {
+      clearInterval(wordsInterval);
+      wordsIntervalIndex = 0;
+      setWords([]);
+      setButtonsVisible(false);
+    };
+  }, [lazyWords]);
+
+  return (
+    <div className={styles.background} style={{ backgroundImage: `url(${image})` }}>
+      <div className={styles.content}>
+        {isButtonsVisible && buttons.map((button) => (
+          <button key={`${button.text}${button.redirectId}`} onClick={handleClick(button.redirectId)}>
+            {button.text}
           </button>
         ))}
+        <p>{words.join(' ')}</p>
       </div>
-    );
-  }
+    </div>
+  );
 }
